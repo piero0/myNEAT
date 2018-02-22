e80913d653f5b41abe6596847e447385cc0c3d02
Piero0 <piero.87@outlook.com>
2018-02-22 21:26:41 +0100

    more changes

---
+++ tests/test01.cpp
@@ -0,0 +1,10 @@
+#include "catch.hpp"
+#include "fact.hpp"
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(0) == 1);
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
