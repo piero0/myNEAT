6aad60d3e30a8df258a43566282c7b8ff97fdfc1
Piero <piero.87@outlook.com>
2018-08-05 22:29:25 +0200

    Another little step

--- ANNFactory.cpp
+++ ANNFactory.cpp
@@ -2,23 +2,46 @@
 
 using namespace pneat;
 
-ANN ANNFactory::createFromGenome(std::shared_ptr<Genome> gnm) {
-    Genes gns = gnm->getGenes();
-    auto idxs = gnm->getNodes().getIdxs();
-
-    //make indecies map
+std::vector<ushort> ANN::createIndexMap(std::vector<ushort>& idxs) {
     auto nodesMap = std::vector<ushort>(idxs[idxs.size()-1]);
     for(size_t i=0; i<idxs.size(); i++)
         nodesMap[idxs[i]] = i;
+    return nodesMap;
+}
+ 
+void ANN::setupFromGenome(Genome& gnm) {
+    Genes gns = gnm.getGenes();
+    auto idxs = gnm.getNodes().getIdxs();
+
+    auto idx2node = this->createIndexMap(idxs);
 
     //now we can use vector indecies instead of nodeIdx
     //as there can be holes between nodeIdxs
+
+    this->initNodes(idxs.size());
 
     for(auto& g: gns) {
         //check if 'from' exist already
         //create or add link
         //and so on...
+
+        auto& netNode = nodes[idx2node[g.fromIdx]];
+
+        netNode.idx = g.toIdx;
+        netNode.links.push_back(g.fromIdx);
+        netNode.weights.push_back(g.weight);
+
     };
 
-    return ANN();
+}
+
+void ANN::activate(std::vector<float>& input) {
+    //put inputs to input nodes and go on...
+    //run a function using picked algorithm
+
+    // this->setupInputNodes(input);
+
+    // auto output = this->recurentActivate();
+    //this->smartActivate();
+    //return output
 }
--- ANNFactory.hpp
+++ ANNFactory.hpp
@@ -1,27 +1,41 @@
 #pragma once
 #include "genome.hpp"
-#include <util.hpp>
+#include "util.hpp"
 
 namespace pneat {
-    template<class T> class annNode {
-        std::vector<T> links;
-        T idx;
-        //type input/output/hidden?
-
+    class annNode {
         public:
-            annNode(T index) {idx = index;}
-            void addLink(T idx) {
+            std::vector<ushort> links;
+            std::vector<float> weights;
+            std::vector<float> inputs;
+            std::vector<float> outputs;
+
+        ushort idx;
+        //type input/output/hidden?
+        //vector<float> weights??
+            annNode() {idx=0;}
+            annNode(ushort index) {idx = index;}
+            void addLink(ushort idx) {
                 links.push_back(idx);
             }
     };
 
     class ANN {
-        std::vector<annNode<ushort>> nodes;
+        std::vector<annNode> nodes;
+
+        void initNodes(size_t num) {
+            nodes = std::vector<annNode>(num);
+        }
+        
+        public:
+            std::vector<ushort> createIndexMap(std::vector<ushort>& idxs);
+            void setupFromGenome(pneat::Genome& gnm);
+            void activate(std::vector<float>& input);
     };
 
-    class ANNFactory {
-    public:
-        ANN createFromGenome(std::shared_ptr<pneat::Genome> gnm);
-        ANN createFromConfig(std::string filename);
-    };
+    // class ANNFactory {
+    // public:
+    //     ANN createFromGenome(std::shared_ptr<pneat::Genome> gnm);
+    //     ANN createFromConfig(std::string filename);
+    // };
 }
